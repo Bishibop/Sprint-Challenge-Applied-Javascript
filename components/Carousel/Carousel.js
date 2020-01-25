b39ed8a4f1a5c4e26436de062cd5d9d35ecdd975
Nicholas Mullen <nwmullen@gmail.com>
2020-01-24 23:58:26 -0500

    buggy carousel

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,95 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+const imageUrls = [
+  "./assets/carousel/mountains.jpeg",
+  "./assets/carousel/computer.jpeg",
+  "./assets/carousel/trees.jpeg",
+  "./assets/carousel/turntable.jpeg" 
+];
+
+let carouselContainerEl = document.querySelector('.carousel-container');
+carouselContainerEl.appendChild(buildCarousel(imageUrls));
+
+
+function buildCarousel(imageUrls) {
+  let carouselEl = document.createElement('div');
+  carouselEl.classList.add('carousel');
+
+  let leftButtonEl = document.createElement('div');
+  leftButtonEl.classList.add('left-button');
+  leftButtonEl.style.zIndex = 1000;
+
+  let imageEls = imageUrls.map(imageUrl => {
+    let imageEl = document.createElement('img');
+    imageEl.src = imageUrl;
+    imageEl.style.position = 'absolute';
+    imageEl.style.display = 'none';
+    return imageEl;
+  });
+  imageEls[0].style.display = 'inline';
+
+  let rightButtonEl = document.createElement('div');
+  rightButtonEl.classList.add('right-button');
+  rightButtonEl.style.zIndex = 1000;
+
+  [leftButtonEl, ...imageEls, rightButtonEl].forEach(element => {
+    carouselEl.appendChild(element);
+  });
+
+  let imageIndex = 0;
+
+  leftButtonEl.addEventListener('click', event => {
+    let newIndex = imageIndex;
+    if ((imageIndex - 1) >= 0) {
+      newIndex = imageIndex - 1;
+    } else {
+      newIndex = imageIndex + 3;
+    }
+    let currentImage = imageEls[imageIndex];
+    let newImage = imageEls[newIndex];
+    gsap.to(currentImage, {
+      duration: 1,
+      right: 1200,
+      onComplete: () => {
+        currentImage.style.display = 'none';
+        currentImage.style.right = null;
+        currentImage.style.left = null;
+      }
+    });
+    newImage.style.display = 'inline';
+    imageIndex = newIndex;
+  });
+
+  rightButtonEl.addEventListener('click', event => {
+    let newIndex = imageIndex;
+    if ((imageIndex + 1) <= 3) {
+      newIndex = imageIndex + 1;
+    } else {
+      newIndex = imageIndex - 3;
+    }
+    let currentImage = imageEls[imageIndex];
+    let newImage = imageEls[newIndex];
+    gsap.to(currentImage, {
+      duration: 1,
+      left: 1200,
+      onComplete: () => {
+        currentImage.style.display = 'none';
+        currentImage.style.right = null;
+        currentImage.style.left = null;
+      }
+    });
+    newImage.style.display = 'inline';
+    imageIndex = newIndex;
+    // imageEls[imageIndex].style.display = 'none';
+    // if ((imageIndex + 1) <= 3) {
+    //   imageIndex += 1;
+    // } else {
+    //   imageIndex -= 3;
+    // }
+    // imageEls[imageIndex].style.display = 'block';
+  });
+
+  return carouselEl;
+}
