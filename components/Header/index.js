ddf37476640856313044e8063d5105911eb8297c
Nicholas Mullen <nwmullen@gmail.com>
2020-01-24 21:31:04 -0500

    header section

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,25 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header() {
+  let headerEl = document.createElement('header');
+
+  let dateEl = document.createElement('span');
+  dateEl.classList.add('date');
+  dateEl.textContent = "SMARCH 28, 2019";
+
+  let h1El = document.createElement('h1');
+  h1El.textContent = "LambdaTimes";
+
+  let tempEl = document.createElement('span');
+  tempEl.classList.add('temp');
+  tempEl.textContent = "98\u00B0";
+
+  [dateEl, h1El, tempEl].forEach(element => {
+    headerEl.appendChild(element);
+  });
+
+  return headerEl;
+}
+
+document.querySelector('.header-container').appendChild(Header());
